73f8043c3c3e3d7189c5f4610b55936c0b1490e9
Natchapon Chitturongaporn <88874631+moocandygumlol@users.noreply.github.com>
2022-12-14 15:50:44 +0700

    checkpoint1

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
+  $startDate = ''
+  $finalDate = ''
+
   def login
     # is_login? if login already cant login again until logout 
     login_again?
@@ -30,6 +33,7 @@ class MainController < ApplicationController
     if session[:logged_in]
       @role = role?
       @id = session[:id]
+      session[:create] = false
     else
       @role = 'guest'
     end
@@ -70,5 +74,35 @@ class MainController < ApplicationController
 
   def topseller
     must_be_logged_in
+    if session[:create]
+      @startDate = $startDate
+      @finalDate = $finalDate
+    end
+  end
+
+  def create_tables
+    $startDate = params[:startdate]
+    $finalDate = params[:finaldate]
+    session[:create] = true
+    redirect_to top_seller_path
+  end
+
+  def buy
+    @amount = params[:amount].to_i
+    $m = Market.where(id: params[:f_id]).first
+    $I = Inventory.new
+    if @amount > $m.quantity
+      redirect_to my_market_path , notice: "the quantity of this goods is not enough"
+    else
+      $m.quantity -= @amount
+      $m.save
+      $I.user_id = session[:id].to_i
+      $I.item_id = $m.item_id
+      $I.seller_id = $m.user_id
+      $I.price = $m.price
+      $I.quantity = @amount
+      $I.save
+      redirect_to my_market_path , notice: "transaction success"
+    end
   end
 end
